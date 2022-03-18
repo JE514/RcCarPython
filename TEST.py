dd7d0bd9297faaa6e50a12f6ae9b90d7f72d3661
OoDone <48130949+OoDone@users.noreply.github.com>
2022-03-18 13:07:45 -0400

    Create TEST.py

---
+++ TEST.py
@@ -0,0 +1,13 @@
+import os     #importing os library so as to communicate with the system
+import time   #importing time library to make Rpi wait because its too impatient 
+os.system ("sudo pigpiod") #Launching GPIO library
+time.sleep(1) # As i said it is too impatient and so if this delay is removed you will get an error
+import pigpio #importing GPIO library
+
+ESC=4  #Connect the ESC in this GPIO pin 
+
+pi = pigpio.pi();
+pi.set_servo_pulsewidth(ESC, 0) 
+
+max_value = 2000 #change this if your ESC's max value is different or leave it be
+min_value = 700  #change this if your ESC's min value is different or leave it be
