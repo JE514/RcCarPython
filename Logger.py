1747ee2972f67c49edd9dd3a307e7a6f4604fada
OoDone <48130949+OoDone@users.noreply.github.com>
2022-04-05 13:48:50 -0400

    Update Logger.py

--- Logger.py
+++ Logger.py
@@ -2,16 +2,9 @@ import sys
 import logging
 import logging.config
 
-#def Logger(): 
-  #logger = logging.getLogger('robotLog')
-  #logger.setLevel(logging.DEBUG)
-  # create file handler which logs even debug messages
-  #fh = logging.FileHandler('robotLog.log')
-  #logger.addHandler(fh)
-  #return logger.getLogger()
 
 def Logger(file_name):
-        formatter = logging.Formatter(fmt='%(asctime)s %(module)s,line: %(lineno)d %(levelname)8s | %(message)s',
+        formatter = logging.Formatter(fmt='%(asctime)s %(module)s  %(levelname)8s | %(message)s',
                                       datefmt='%Y/%m/%d %H:%M:%S') # %I:%M:%S %p AM|PM format
         logging.basicConfig(filename = '%s.log' %(file_name),format= '%(asctime)s %(module)s,line: %(lineno)d %(levelname)8s | %(message)s',
                                       datefmt='%Y/%m/%d %H:%M:%S', filemode = 'w', level = logging.INFO)
