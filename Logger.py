7676b17d7183766db71611bc2728b9d20629353c
OoDone <48130949+OoDone@users.noreply.github.com>
2022-04-05 11:56:51 -0400

    Update Logger.py

--- Logger.py
+++ Logger.py
@@ -1,1 +1,11 @@
 
+def logger(msg):
+  print(msg)
+  #log to file
+  #log to controller console
+  
+def logger(msg, var):
+  data = msg.replace("{}", var)
+  print(data)
+  #log to file
+  #log to controller console
