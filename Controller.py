30f38d7e494633e306d4edf9c7a49cdeb01f3ee4
OoDone <48130949+OoDone@users.noreply.github.com>
2022-04-06 08:21:47 -0400

    Update Controller.py

--- Controller.py
+++ Controller.py
@@ -27,7 +27,7 @@ def return_data():
             return data
     except OSError:
         pass
-bluetoothAddress = "DC:A6:32:6B:38:BD"
+bluetoothAddress = "B8:27:EB:D6:57:CE" #DC:A6:32:6B:38:BD
 stickDeadband = 5
 
 
