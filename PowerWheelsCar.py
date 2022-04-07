02afb9e764b9363209029ba724edbecf9844bf48
OoDone <48130949+OoDone@users.noreply.github.com>
2022-04-06 21:10:52 -0400

    Update PowerWheelsCar.py

--- PowerWheelsCar.py
+++ PowerWheelsCar.py
@@ -142,14 +142,20 @@ while(1):
                 speed = float(speed)
                 direction = float(direction)
             except:
-                speed = 0
-                direction = 0
+                speed = 0.0
+                direction = 0.0
                 logger.warn("Exception: speed or direction not a number")
             if speed > 0:#0
                 pi.set_servo_pulsewidth(ESC, speed*5+motorNeutralSpeed)
             elif speed < 0: #0
                 pi.set_servo_pulsewidth(ESC, motorNeutralSpeed + speed * 5)
             else:
+                logger.info("ESC: SET PULSEWIDTH TO 0")
+                logger.info("ESC: SET PULSEWIDTH TO 0")
+                logger.info("ESC: SET PULSEWIDTH TO 0")
+                logger.info("ESC: SET PULSEWIDTH TO 0")
+                logger.info("ESC: SET PULSEWIDTH TO 0")
+                logger.info("ESC: SET PULSEWIDTH TO 0")
                 pi.set_servo_pulsewidth(ESC, 0)
             if direction < 0:
                 directionPosition = -direction * directionTicksPer * 9.36 + 1489 # TEMP  
