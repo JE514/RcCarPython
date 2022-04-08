c5301f534ea64bcebbda36a89d80acbef55aac31
OoDone <48130949+OoDone@users.noreply.github.com>
2022-04-08 13:34:39 -0400

    Update MainAuton.py

--- autonomous/MainAuton.py
+++ autonomous/MainAuton.py
@@ -28,25 +28,36 @@ def getAutonEnabled():
 def enableAuton(enabled, mode=0):
   if getAutonEnabled() == True and enabled == True:
     logger.info("Auton: Already Enabled.")
+    sock.send("Auton: Already Enabled.")
   elif getAutonEnabled() == False and enabled == False:
     logger.info("Auton: Already Disabled.")
+    sock.send("Auton: Already Disabled.")
   elif enabled == True:
     if autonMode != 0:
       autonMode = mode
       PowerWheels.setAutonMode(mode)
     logger.info("Auton: Enabling Autonomous In Mode " + autonMode)
+    sock.send("Auton: Enabling Autonomous In Mode " + autonMode)
   else:
     logger.info("Auton: Disabling Autonomous...")
+    sock.send("Auton: Disabling Autonomous...")
 
   
 while autonEnabled:
   distance = ultrasonicRead()
-  
-  if autonMode == 1: #simple auton mode 
-    if getDriveSpeed() != 20.0:
-      driveMotor(20.0)
-    if distance < 10:
-      logger.info("Auton: Distance within 10, Stopping Robot...")
-      driveMotor(0.0)
+  if disconnected == False:
+    
+    if autonMode == 1: #simple auton mode 
+      if getDriveSpeed() != 20.0:
+        driveMotor(20.0)
+      if distance < 10:
+        logger.info("Auton: Distance within 10, Stopping Robot...")
+        driveMotor(0.0)
+   if autonMode == 2: #simple(But Fast) auton mode 
+      if getDriveSpeed() != 50.0:
+        driveMotor(50.0)
+      if distance < 10:
+        logger.info("Auton: Distance within 10, Stopping Robot...")
+        driveMotor(0.0)
     
 
