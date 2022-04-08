8b516e03f04d7b31c7da3ce69a48f2d95d73efd6
OoDone <48130949+OoDone@users.noreply.github.com>
2022-04-08 08:33:18 -0400

    Update PowerWheelsCar.py

--- PowerWheelsCar.py
+++ PowerWheelsCar.py
@@ -28,6 +28,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(buzzerPin, GPIO.OUT)
 GPIO.output(buzzerPin, GPIO.LOW)
 bd_addr = "DC:A6:32:6B:38:BD"  #"B8:27:EB:D6:57:CE" 
+#B8:27:EB:6B:AB:4B
 uuid = "42b58f76-b26d-11ea-b733-cb205305bc99"
 port = 1
 
